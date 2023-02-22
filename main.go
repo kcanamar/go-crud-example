ab97a5993b64de67d1dd6bb2fc8e3ec7d343ea61
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-02-22 13:34:01 -0800

    adds root route to api

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hi Kyle")
+	// setup a gin router
+	r := gin.Default()
+	// Declare home get route
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello from the other side!",
+		})
+	})
+	// setup our server listener
+	r.Run()
 }
