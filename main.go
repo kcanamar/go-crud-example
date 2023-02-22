315537f1e3bf366482c1a3868a62090340aa5139
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-02-22 13:44:06 -0800

    links .env file to main

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"log"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	err := godotenv.Load()
+	
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 	// setup a gin router
