f25ec4ad2d70692c81acfb34eb986125f6da9ae9
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-02-22 13:55:01 -0800

    creates seperate file for loading env content

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"log"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	"github.com/kcanamar/go-crud/initializers"
 )
 
 func init() {
-	err := godotenv.Load()
-	
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	initializers.LoadEnv()
 }
 
 func main() {
