e339dbaf226e624b87ae863d0722ccb8f43375d9
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-02-25 11:12:38 -0800

    deployment

--- initializers/loadEnv.go
+++ initializers/loadEnv.go
@@ -2,12 +2,21 @@ package initializers
 
 import (
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	err := godotenv.Load()
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	environmentPath := filepath.Join(dir, ".env")
+	err = godotenv.Load(environmentPath)
 	
 	if err != nil {
 		log.Fatal("Error loading .env file")
